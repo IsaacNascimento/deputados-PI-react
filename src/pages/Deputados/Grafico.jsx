0dbfe210dfb32e09d4aba44ea85e0670716e211b
maycon <mayconborgesgato717@gmail.com>
2022-06-05 11:07:41 -0300

    mudanças nos graficos

--- src/pages/Deputados/Grafico.jsx
+++ src/pages/Deputados/Grafico.jsx
@@ -1,43 +1,41 @@
-import { useState } from "react";
 import BarChart from "../../components/BarChart";
 import PieChart from "../../components/PieChart";
 import LineChart from "../../components/LineChart";
-import { UserData } from "../../Data";
 
 function Grafico() {
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
-    datasets: [
-      {
-        label: "Users Gained",
-        data: UserData.map((data) => data.userGain),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
+  const data = {
+    labels: [
+      '2018', 
+      '2019', // aqui fica os nomes da parte de baixo dos graficos
+      '2021',
+      '2022'
     ],
-  });
-
-  // Isaac o Userdata ta sendo importado ele vem do arquivo Data.js
-  // userdata Com o U minusculo vem de dentro const a baixo da function
-  // dentro da pasta components tem 3 arquivos que são os graficos.
-
+    datasets: [{
+      label: 'Dados de Gastos',
+      data: [300, 50, 100, 200], // aqui dentro a gente faz um loop por ano, exemplo: deputados.map((item) => item.ano)
+      backgroundColor: [
+        'rgb(255, 0, 0)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)',
+        'rgb(0, 255, 127)'
+      ],
+      hoverOffset: 4
+    }]
+  };
+ // a const de data estou chamando dentro de chartData 
   return (
     <div >
-      <div className="centralx" style={{ width: 700 }}>
-        <BarChart chartData={userData} />
+      <div className="text-center">
+        <h1>Gastos feito por Deputados</h1>
       </div>
       <div style={{ width: 700 }}>
-        <LineChart chartData={userData} />
+        <BarChart chartData={data} /> 
       </div>
       <div style={{ width: 700 }}>
-        <PieChart chartData={userData} />
+        <LineChart chartData={data} />
+      </div>
+      <div style={{ width: 700 }}>
+        <PieChart chartData={data} />
       </div>
     </div>
   );
