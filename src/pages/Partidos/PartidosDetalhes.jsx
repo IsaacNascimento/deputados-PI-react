1ac864070b17dbb29ef824da98932fdd5f2a613b
maycon <mayconborgesgato717@gmail.com>
2022-06-07 15:00:52 -0300

    colocado cards em partidos detalhes

--- src/pages/Partidos/PartidosDetalhes.jsx
+++ src/pages/Partidos/PartidosDetalhes.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Card, Col, Row } from "react-bootstrap";
+import { Card, Col } from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
 import { getPartidosByID } from "../../service/partidos";
 
 export const PartidosDetalhes = () => {
   const [partido, setPartido] = useState([]);
-
+  console.log(partido)
   const [partidoMembro, setPartidoMembro] = useState([]);
   console.log(partidoMembro)
   const params = useParams();
@@ -18,23 +18,21 @@ export const PartidosDetalhes = () => {
     <div>
         <div className="container-detalhes">
           <div className="texto1">
-            <h1>{partido.sigla}</h1>
             <img src={partido.urlLogo} alt="logo" />
-            <img src={partido.uriPartido}  alt="" />
           </div>
         </div>
         <div>
         {partidoMembro.map((item) => (
-              <Col className="mb-3" md={2} key={item.id}>
-                <Link to={"/atores/" + item.id}>
-                  <Card title={item.name}>
-                    <Card.Img
-                      variant="top"
-                      src={item.urlFoto}/>
-                  </Card>
-                </Link>
-              </Col>
-            ))}
+          <Col className="mb-3" md={2} key={item.id}>
+            <Link to={"/detalhes/" + item.id}>
+              <Card title={item.nome}>
+                <Card.Img
+                  variant="top"
+                  src={item.urlFoto}/>
+              </Card>
+            </Link>
+          </Col>
+        ))}
         </div>
     </div>
   )
